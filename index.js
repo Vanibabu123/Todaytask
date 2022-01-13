0227de9b87803eb22ceee203da4a786d69a89f09
Vanibabu123 <96285039+Vanibabu123@users.noreply.github.com>
2022-01-13 14:13:10 +0530

    Image task uploaded

---
+++ index.js
@@ -0,0 +1,50 @@
+const images = ["https://www.google.com/imgres?imgurl=https%3A%2F%2Fimages.all-free-download.com%2Fimages%2Fgraphiclarge%2Fautumn_landscape_199954.jpg&imgrefurl=https%3A%2F%2Fall-free-download.com%2Ffree-photos%2Fbeautiful-landscape.html&tbnid=lkZlaaoHKeZxzM&vet=12ahUKEwjJvPiBoq71AhW7GbcAHXAcCrwQMygFegUIARDLAQ..i&docid=gaIMqDDjkLIsWM&w=600&h=400&itg=1&q=landscape%20images%20download&ved=2ahUKEwjJvPiBoq71AhW7GbcAHXAcCrwQMygFegUIARDLAQ","https://www.google.com/imgres?imgurl=https%3A%2F%2Fwallpaperaccess.com%2Ffull%2F112714.jpg&imgrefurl=https%3A%2F%2Fwallpaperaccess.com%2Flandscape&tbnid=GEwCM4tgeDnoJM&vet=12ahUKEwjJvPiBoq71AhW7GbcAHXAcCrwQMygEegUIARDJAQ..i&docid=0GSG3FqHWkRXeM&w=1920&h=1080&itg=1&q=landscape%20images%20download&ved=2ahUKEwjJvPiBoq71AhW7GbcAHXAcCrwQMygEegUIARDJAQ", "https://www.google.com/imgres?imgurl=https%3A%2F%2Fwallpaperaccess.com%2Ffull%2F112714.jpg&imgrefurl=https%3A%2F%2Fwallpaperaccess.com%2Flandscape&tbnid=GEwCM4tgeDnoJM&vet=12ahUKEwjJvPiBoq71AhW7GbcAHXAcCrwQMygEegUIARDJAQ..i&docid=0GSG3FqHWkRXeM&w=1920&h=1080&itg=1&q=landscape%20images%20download&ved=2ahUKEwjJvPiBoq71AhW7GbcAHXAcCrwQMygEegUIARDJAQ", "https://www.google.com/imgres?imgurl=https%3A%2F%2Fcdn.wallpapersafari.com%2F73%2F33%2FRtHpg1.jpg&imgrefurl=https%3A%2F%2Fwallpapersafari.com%2Fw%2FRtHpg1&tbnid=Lq1kqWqVLZQMBM&vet=12ahUKEwjJvPiBoq71AhW7GbcAHXAcCrwQMygIegUIARDRAQ..i&docid=m0tPORCNeOi9FM&w=1440&h=1280&itg=1&q=landscape%20images%20download&ved=2ahUKEwjJvPiBoq71AhW7GbcAHXAcCrwQMygIegUIARDRAQ", "https://www.google.com/imgres?imgurl=https%3A%2F%2Fwallpaper-house.com%2Fdata%2Fout%2F5%2Fwallpaper2you_45804.jpg&imgrefurl=https%3A%2F%2Fwallpaper-house.com%2Fgroup%2Fnatural-landscape-wallpaper-hd%2Findex.php&tbnid=uZmIbVdNQpclLM&vet=12ahUKEwjJvPiBoq71AhW7GbcAHXAcCrwQMygKegUIARDVAQ..i&docid=aAykFnrXeD3P8M&w=1920&h=1200&itg=1&q=landscape%20images%20download&ved=2ahUKEwjJvPiBoq71AhW7GbcAHXAcCrwQMygKegUIARDVAQ", "https://www.google.com/imgres?imgurl=https%3A%2F%2Fmedia.istockphoto.com%2Fphotos%2Fhot-air-balloons-flying-over-the-botan-canyon-in-turkey-picture-id1297349747%3Fb%3D1%26k%3D20%26m%3D1297349747%26s%3D170667a%26w%3D0%26h%3DoH31fJty_4xWl_JQ4OIQWZKP8C6ji9Mz7L4XmEnbqRU%3D&imgrefurl=https%3A%2F%2Funsplash.com%2Fimages%2Fnature%2Flandscape&tbnid=ukMCie7JytCd0M&vet=12ahUKEwjJvPiBoq71AhW7GbcAHXAcCrwQMygNegUIARDbAQ..i&docid=ww-QvRtiCqpDeM&w=495&h=348&itg=1&q=landscape%20images%20download&ved=2ahUKEwjJvPiBoq71AhW7GbcAHXAcCrwQMygNegUIARDbAQ"];
+for(let image of images){
+
+const imgAll = document.createElement("img");
+
+
+
+imgAll.setAttribute("src",image);
+
+ imgAll.setAttribute("class","img-sec")
+
+// const favImag = document.createElement("img");
+
+// favImag.innerText= image;
+
+// imgAll.append(favImag);
+
+document.body.append(imgAll);
+}
+
+
+
+
+const india = "https://www.google.com/imgres?imgurl=https%3A%2F%2Fcdn.pixabay.com%2Fphoto%2F2016%2F08%2F24%2F17%2F07%2Findia-1617463__340.png&imgrefurl=https%3A%2F%2Fpixabay.com%2Fimages%2Fsearch%2Findia%2520flag%2F&tbnid=9owhUStUiYF0CM&vet=12ahUKEwj99My8qa71AhVHwDgGHbx0DxMQMygAegUIARDLAQ..i&docid=EI4ZS6swSX7qiM&w=508&h=340&itg=1&q=download%20india%20flag&ved=2ahUKEwj99My8qa71AhVHwDgGHbx0DxMQMygAegUIARDLAQ";
+const  flags = document.createElement("img");
+flags.setAttribute("src",india);
+flags.setAttribute("class","india");
+ document.body.append(flags);
+
+
+const letter = document.createElement("h2");
+letter.innerText = "India";
+document.body.append(letter);
+
+
+
+const other = document.createElement("h4");
+other.innerText = "Population : 2,45,4545,45";
+document.body.append(other);
+
+
+
+const regi = document.createElement("h4");
+regi.innerText = "Region : India";
+document.body.append(regi);
+
+
+const capi = document.createElement("h4");
+capi.innerText = "Capital : India";
+document.body.append(capi);
